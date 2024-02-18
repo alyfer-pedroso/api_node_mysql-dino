0194bdeba7fc0e23f2cc5c5a4747fb30e535ff59
alyfer-pedroso <alyferleandroaraujopedroso@gmail.com>
2024-02-18 20:51:26 -0300

    vercel

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const routes = require("./routes");
 const server = express();
 server.use(cors());
 server.use(bodyParser.urlencoded({ extended: false }));
-server.use("", routes);
+server.use("/apí", routes);
 
 server.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
