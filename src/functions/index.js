e7d1cbfe662239a5b1d47c17d935b5a00a60120e
Alyfer Pedroso <138719366+alyfer-pedroso@users.noreply.github.com>
2024-05-23 20:31:41 -0300

    fix: functions/authToken

---
+++ src/functions/index.js
@@ -0,0 +1,1 @@
+export { authToken } from "./authToken";
--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,9 @@
+import { authToken } from "./functions";
+
 const express = require("express");
 const router = express.Router();
 
 const UsersController = require("./controllers/UsersController");
-
-const authToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-
-  if (token == null) return res.sendStatus(401);
-
-  const expectedToken = process.env.TOKEN;
-  if (token !== expectedToken) return res.sendStatus(403);
-
-  next();
-};
 
 //#region get
 router.get("/users", authToken, UsersController.searchAll);
