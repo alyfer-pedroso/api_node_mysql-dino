10e2330e15af0242b188ca7d705e39a80b9f8151
alyfer-pedroso <alyfer@mbr.net.br>
2024-05-26 14:54:22 -0300

    refactor: UsersController functions

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -14,13 +14,12 @@ module.exports = {
   verifyLogin: async (req, resp) => {
     const { email, password } = req.body;
     try {
-      if (email && password) {
-        const result = await UsersService.verifyLogin(email, password);
-        result.length > 0
-          ? resp.json(new Sucessful({ email: result[0].email, password: result[0].password }, "Login efetuado com sucesso!"))
-          : resp.json(new Error("Email ou Senha incorretos!", "Login inválido!"));
-      }
-      if (!email || !password) resp.json(new Error("Preencha todos os campos!", "Login inválido"));
+      if (!email || !password) return resp.json(new Error("Preencha todos os campos!", "Login inválido"));
+
+      const result = await UsersService.verifyLogin(email, password);
+      result.length > 0
+        ? resp.json(new Sucessful({ email: result[0].email, password: result[0].password }, "Login efetuado com sucesso!"))
+        : resp.json(new Error("Email ou Senha incorretos!", "Login inválido!"));
     } catch (error) {
       resp.json(new Error(error.message));
     }
@@ -29,74 +28,60 @@ module.exports = {
   register: async (req, resp) => {
     const { email, user, password } = req.body;
     try {
-      if (email && user && password) {
-        const verifyIfExist = await UsersService.verifyEmail(email);
-        if (verifyIfExist.length > 0) return resp.json(new Error("Esse email já está sendo usado.", "Registro inválido!"));
-        const register = await UsersService.register(email, user, password);
-        resp.json(new Sucessful({ ...register, registration_date: new Date() }, "Usuário cadastrado com sucesso!"));
-      }
+      if (!email || !user || !password) return resp.json(new Error("Preencha todos os campos!", "Registro inválido"));
 
-      if (!email || !user || !password) resp.json(new Error("Preencha todos os campos!", "Registro inválido"));
+      const verifyIfExist = await UsersService.verifyEmail(email);
+      if (verifyIfExist.length > 0) return resp.json(new Error("Esse email já está sendo usado.", "Registro inválido!"));
+
+      await UsersService.register(email, user, password);
+      resp.json(new Sucessful({ ...req.body, registration_date: new Date() }, "Usuário cadastrado com sucesso!"));
     } catch (error) {
       resp.json(new Error(error.message));
     }
   },
 
   deleteUser: async (req, resp) => {
-    let json = { status: "", data: {} };
-    await UsersService.deleteUser(req.params.id);
-    resp.json(json);
+    const { id } = req.params;
+    try {
+      if (!id) return resp.json(new Error("Preencha todos os campos!", "Delete inválido"));
+
+      const exist = await UsersService.verifyID(id);
+      if (exist.length < 1) return resp.json(new Error(`Não foi possível encontrar o ID: ${id}`, "Delete inválido"));
+
+      await UsersService.deleteUser(id);
+      resp.json(new Sucessful({ id }, "Usuário deletado com sucesso!"));
+    } catch (error) {
+      resp.json(new Error(error.message));
+    }
   },
 
   changePassword: async (req, resp) => {
-    let json = { status: "", data: {} };
-    let password = req.body.password;
-    let id = req.body.id;
+    const { id, password } = req.body;
+    try {
+      if (!id || !password) return resp.json(new Error("Preencha todos os campos!", "Alteração inválida"));
 
-    if (password && id) {
       const exist = await UsersService.verifyID(id);
-      if (exist.length < 1) {
-        json.status = `Não foi possível encontrar o ID: ${id}`;
-        return resp.json(json);
-      }
+      if (exist.length < 1) return resp.json(new Error(`Não foi possível encontrar o ID: ${id}`, "Alteração inválida"));
 
       await UsersService.changePassword(password, id);
-      json.status = "Senha atualizada com sucesso!";
-      json.data = { id, password };
-    } else {
-      json.status = "A alteração não pode ser efetuada.";
+      resp.json(new Sucessful({ id, password }, "Senha atualizada com sucesso!"));
+    } catch (error) {
+      resp.json(new Error(error.message));
     }
-
-    if (!id || !password) {
-      json.status = "Preencha todos os campos!";
-    }
-
-    resp.json(json);
   },
 
   setOnline: async (req, resp) => {
-    let json = { status: "", data: {} };
-    let online = req.body.online;
-    let id = req.body.id;
+    const { online, id } = req.body;
+    try {
+      if (!id || !online) return resp.json(new Error("Preencha todos os campos!", "Alteração inválida"));
 
-    if (online && id) {
       const exist = await UsersService.verifyID(id);
-      if (exist.length < 1) {
-        json.status = `Não foi possível encontrar o ID: ${id}`;
-        return resp.json(json);
-      }
+      if (exist.length < 1) return resp.json(new Error(`Não foi possível encontrar o ID: ${id}`, "Alteração inválida"));
 
       await UsersService.setOnline(online, id);
-      json.status = "A alteração foi efetuada com sucesso!";
-      json.data = { id, online };
-    } else {
-      json.status = "A alteração não pode ser efetuada.";
+      resp.json(new Sucessful({ id, online }, "Alteração feita com sucesso!"));
+    } catch (error) {
+      resp.json(new Error(error.message));
     }
-
-    if (!id || !online) {
-      json.status = "Preencha todos os campos!";
-    }
-
-    resp.json(json);
   },
 };
